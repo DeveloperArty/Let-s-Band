9e7179190e087548d8bced782ef24268daab90d4
DeveloperArty <apple_dev.arty@mail.ru>
2016-12-22 23:57:34 +0300

    query are kind of made, but still sh.ty

--- Let's Band/Cloud.swift
+++ Let's Band/Cloud.swift
@@ -14,28 +14,32 @@ class Cloud {
     // MARK: - Properties
     let container: CKContainer
     let publicDB: CKDatabase
-    let privateDB: CKDatabase
     // Log In 
-    var noUserWithThisNickname: Bool
+    var userNicknameFound: Bool
+    var userPasswordFound: Bool
     
     
     // MARK: - Initializers
     init() {
         container = CKContainer.default()
         publicDB = container.publicCloudDatabase
-        privateDB = container.privateCloudDatabase
-        noUserWithThisNickname = true
+        userNicknameFound = false
+        userPasswordFound = false
     }
+    
     
     // MARK: - Cloud Methods
     func registerNewUser(name: String, surname: String, dateOfBirth: Date, nickname: String, password: String, mail: String) {
         
         // saving public data
-        let publicDataRecord = CKRecord(recordType: "publicUsersData")
+        let publicDataRecord = CKRecord(recordType: "publicUserData")
         publicDataRecord["Name"] = name as CKRecordValue?
         publicDataRecord["Surname"] = surname as CKRecordValue?
         publicDataRecord["DateOfBirth"] = dateOfBirth as CKRecordValue?
         publicDataRecord["Nickname"] = nickname as CKRecordValue?
+        publicDataRecord["Password"] = password as CKRecordValue?
+        publicDataRecord["Mail"] = mail as CKRecordValue?
+
         publicDB.save(publicDataRecord, completionHandler: { record , error in
             if error == nil {
                 print("public data saved correctly")
@@ -44,42 +48,44 @@ class Cloud {
             }
         })
         
-        //saving private data 
-        let privateDataRecord = CKRecord(recordType: "privateUsersData")
-        privateDataRecord["Password"] = password as CKRecordValue?
-        privateDataRecord["Mail"] = mail as CKRecordValue?
-        privateDB.save(privateDataRecord, completionHandler: { record , error in
-            if error == nil {
-                print("private data saved correctly")
-            } else {
-                print("an error occured while saving private data; error: \(error?.localizedDescription)")
-            }
-        })
     }
     
     func logIn(userNickname: String, userPassword: String) {
 
         // searching for user's data in the cloud
-        let publicPredicate = NSPredicate(format: "Nickname = %@", userNickname)
-        let publicDBQuery = CKQuery(recordType: "publicUsersData", predicate: publicPredicate)
-            publicDB.perform(publicDBQuery, inZoneWith: nil, completionHandler: { recordFound, error in
-                 if error != nil {
-                    print("an error occured while performing a nickname query")
-                 } else {
-                    guard recordFound?.isEmpty == false else {
-                        print("no users with requested nickname found")
-                        return
-                    }
-                    print("user with requested nickname found, \(recordFound)")
-                    self.noUserWithThisNickname = false
-                }
-            })
         
-//        let privatePredicate = NSPredicate(value: true)
-//        let privateDBQuery = CKQuery(recordType: "privateUsersData", predicate: privatePredicate)
-//        privateDB.perform(privateDBQuery, inZoneWith: nil, completionHandler: { recordFound, error in
-//            
-//        })
+        // searching for a nickname
+        let nicknamePredicate = NSPredicate(format: "Nickname = %@", userNickname)
+        let nicknameQuery = CKQuery(recordType: "publicUserData", predicate: nicknamePredicate)
+        publicDB.perform(nicknameQuery, inZoneWith: nil, completionHandler: { recordsFound, error in
+            if error != nil {
+                print("an error occured while performing a nickname query, error: \(error?.localizedDescription)")
+            } else {
+                guard recordsFound?.isEmpty == false else {
+                    print("no users with requested nickname found")
+                    return
+                }
+                print("user with requested nickname found")
+                self.userNicknameFound = true
+            }
+        })
+        
+        // searching for a password
+        let passwordPredicate = NSPredicate(format: "Password = %@", userPassword)
+        let passwordQuery = CKQuery(recordType: "publicUserData", predicate: passwordPredicate)
+        publicDB.perform(passwordQuery, inZoneWith: nil, completionHandler: { recordsFound, error in
+            if error != nil {
+                print("an error occured while performing a password query")
+            } else {
+                guard recordsFound?.isEmpty == false else {
+                    print("no matching passwords found")
+                    return
+                }
+                print("requested password found")
+                self.userPasswordFound = true 
+            }
+        })
+        
     }
     
 }
