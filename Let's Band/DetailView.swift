6400eaa3b5cb202390dc6f025ccb693e0c9210fe
«DeveloperArty» <apple_dev.arty@mail.ru>
2017-06-29 21:18:30 +0300

    some kinda commit

---
+++ Let's Band/DetailView.swift
@@ -0,0 +1,15 @@
+//
+//  DetailView.swift
+//  Let's Band
+//
+//  Created by Artem Pavlov on 15.01.17.
+//  Copyright © 2017 Den prod. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    
+
+}
---
+++ Let's Band/OtherUserAnnotation.swift
@@ -0,0 +1,16 @@
+//
+//  OtherUserAnnotation.swift
+//  Let's Band
+//
+//  Created by Artem Pavlov on 10.01.17.
+//  Copyright © 2017 Den prod. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class OtherUserAnnotation: MKPointAnnotation {
+
+    var imageName: String? 
+    
+}
